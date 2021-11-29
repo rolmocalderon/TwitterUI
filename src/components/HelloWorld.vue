2a1392e0309948fad68a65291609b76cc76a6f56
Ruben del Olmo <R.OlmoCalderon@gmail.com>
2021-11-30 00:17:50 +0100

    Add chart

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="top">
+    <template v-if="loading">
+        <div>Spinner</div>
+    </template>
+
+    <LineChart v-if="!loading" :labels="labels" :dataset="data" :minValue="minValue" :maxValue="maxValue"></LineChart>
   </div>
+  
 </template>
 
 <script>
+import axios from 'axios'
+import LineChart from "./LineChart.vue"
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    LineChart
+  },
+  data() {
+    return {
+      loading: true,
+      labels: [],
+      data: [],
+      minValue: 0,
+      maxValue: 15000
+    }
+  },
+  created() {
+      this.getDataFromApi()
+  },
+  methods: {
+      getDataFromApi() {
+          axios.get('http://localhost:3100/getFollowers')
+          .then(response => {
+              var data = response.data.map(x => x.value);
+              this.labels = response.data.map(x => x.date);
+              this.data = data;
+              this.minValue = Math.min(...data) - 500;
+              this.maxValue = Math.max(...data) + 500;
+              this.loading = false;
+          })
+          .catch(error => {
+              this.loading = false
+              console.log(error)
+          })
+      }
   }
 }
 </script>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,42 @@
+<script>
+import { Line } from 'vue3-chart-v2'
+
+export default {
+  extends: Line,
+  props: {
+      labels: Array,
+      dataset: Array,
+      minValue: Number,
+      maxValue: Number
+  },
+  mounted() {
+    this.renderChart(
+      {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Followers",
+            data: this.dataset,
+            backgroundColor: "transparent",
+            borderColor: "rgba(1, 116, 188, 0.50)",
+            pointBackgroundColor: "rgba(171, 71, 188, 1)"
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    suggestedMin: this.minValue,
+                    suggestedMax: this.maxValue
+                }
+            }]
+        },
+      }
+    );
+  }
+};
+</script>
